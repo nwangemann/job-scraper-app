85e135d5b6882893e6e594b2eb6e84b2dc8bdbf9
Ammon Murray <ammonmurray1@gmail.com>
2020-03-30 15:27:43 -0700

    updated new changes

--- server/scrapers/indeedScraper.js
+++ server/scrapers/indeedScraper.js
@@ -1,58 +1,62 @@
-const cheerio = require('cheerio');
-const request = require('request-promise');
-const Nightmare = require('nightmare');
+const cheerio = require("cheerio");
+const request = require("request-promise");
+const Nightmare = require("nightmare");
 
-
-const nightmare = Nightmare({ show: true })
-const url = 'https://www.indeed.com'
-let theData = []
+const nightmare = Nightmare({ show: true });
+const url = "https://www.indeed.com";
 
 nightmare
-    .goto(url)
-    .wait('body')
-    .click('input#text-input-what')
-    .type('input#text-input-what', 'Web Developer')
-    .click('input#text-input-where')
-    .type('input#text-input-where', '')
-    .click('button.icl-WhatWhere-button')
-    .wait('div.jobsearch-SerpJobCard')
-    .evaluate(() => document.querySelector('body').innerHTML)
-    .end()
-    .then(response => {
-        theData.push(getData(response))
-        console.log(getData(response));
-    }).catch(err => {
-        console.log(err);
-    })
+  .goto(url)
+  .wait("body")
+  .click("input#text-input-what")
+  .type("input#text-input-what", "Web Developer")
+  .click("input#text-input-where")
+  .type("input#text-input-where", "")
+  .click("button.icl-WhatWhere-button")
+  .wait("div.jobsearch-SerpJobCard")
+  .evaluate(() => document.querySelector("body").innerHTML)
+  .end()
+  .then(response => {
+    console.log(getData(response));
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 let getData = html => {
-    data = [];
-    const $ = cheerio.load(html)
-    $('div.jobsearch-SerpJobCard').each((row, raw_element) => {
-        let title = $(raw_element).find('div.title a').attr('title');
-        let link = $(raw_element).find('div.title a').attr('href')
-        let company = $(raw_element).find('div.sjcl div span.company').text();
-        let location = $(raw_element).find('div.sjcl div.recJobLoc').attr('data-rc-loc')
-        let description = $(raw_element).find('div.summary ul li').text()
-        let date = $(raw_element).find('span.date').text()
+  data = [];
+  const $ = cheerio.load(html);
+  $("div.jobsearch-SerpJobCard").each((row, raw_element) => {
+    let title = $(raw_element)
+      .find("div.title a")
+      .attr("title");
+    let link = $(raw_element)
+      .find("div.title a")
+      .attr("href");
+    let company = $(raw_element)
+      .find("div.sjcl div span.company")
+      .text();
+    let location = $(raw_element)
+      .find("div.sjcl div.recJobLoc")
+      .attr("data-rc-loc");
+    let description = $(raw_element)
+      .find("div.summary ul li")
+      .text();
+    let date = $(raw_element)
+      .find("span.date")
+      .text();
 
-
-
-        if(title) {
-            data.push({
-                title: title,
-                link: link,
-                company: company,
-                location: location,
-                description: description,
-                date: date
-
-            })
-        }
-    })
-    return data
-}
-
-module.exports = {
-    theData: theData
-}
+    if (title) {
+      data.push({
+        title: title,
+        link: link,
+        company: company,
+        location: location,
+        description: description,
+        date: date
+      });
+    }
+  });
+  console.log("data", data);
+  return data;
+};
