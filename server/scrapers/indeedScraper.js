cd2293b665bb0ffcf7129992f56daf04e4f7335f
Ammon Murray <ammonmurray1@gmail.com>
2020-03-30 12:24:40 -0700

    backend scrapes data from indeed

--- server/scrapers/indeedScraper.js
+++ server/scrapers/indeedScraper.js
@@ -27,12 +27,24 @@ let getData = html => {
     data = [];
     const $ = cheerio.load(html)
     $('div.jobsearch-SerpJobCard').each((row, raw_element) => {
-        let title = $(raw_element).find('div.title a').text();
+        let title = $(raw_element).find('div.title a').attr('title');
         let link = $(raw_element).find('div.title a').attr('href')
+        let company = $(raw_element).find('div.sjcl div span.company').text();
+        let location = $(raw_element).find('div.sjcl div.recJobLoc').attr('data-rc-loc')
+        let description = $(raw_element).find('div.summary ul li').text()
+        let date =$(raw_element).find('span.date').text()
+
+
+
         if(title) {
             data.push({
                 title: title,
-                link: link
+                link: link,
+                company: company,
+                location: location,
+                description: description,
+                date: date
+
             })
         }
     })
