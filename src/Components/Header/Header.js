baa8d46b41e3ec62133f75c5e8f669b3f2b53c0d
Ammon Murray <ammonmurray1@gmail.com>
2020-04-02 14:29:10 -0700

    color1

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useSelector} from 'react-redux'
-import "./Header.css";
+import "./Header.scss";
 import { Link } from "react-router-dom";
 import SubHeader from './SubHeader'
 
