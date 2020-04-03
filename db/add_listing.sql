a617a108b25d96fe7b0a329a7cee0d2bda92883c
Ammon Murray <ammonmurray1@gmail.com>
2020-04-03 14:59:16 -0700

    saving files

--- db/add_listing.sql
+++ db/add_listing.sql
@@ -1,13 +1,7 @@
 INSERT INTO jobs
-<<<<<<< HEAD
-(user_id, title, company, description, location, date, link)
-VALUES
-($1, $2, $3, $4, $5, $6, $7);
-=======
 (user_id, title, company, description, location, date, link, website)
 VALUES
 ($1, $2, $3, $4, $5, $6, $7, $8);
->>>>>>> fdc288e5ab8455e7de00543e2377e247054a2fb7
 
 SELECT * FROM jobs
 WHERE user_id = $1;
