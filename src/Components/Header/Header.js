b94fa0cfe0a272feecb578b01ea90ae8d27d1d67
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-04-03 11:12:19 -0700

    changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,7 +9,7 @@ function Header(){
   
     return (
       <div  className="stickyHeader">
-        <Link className="links" to="/">Job Scraper</Link>
+        <Link className="links" to="/">Get Busy</Link>
        
           
         <div>
