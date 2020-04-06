5638cf98739ed05362965509217cc827900ceb8d
Ammon Murray <ammonmurray1@gmail.com>
2020-04-06 10:45:37 -0700

    fixed errors on indeedScaper.js

--- server/scrapers/indeedScraper.js
+++ server/scrapers/indeedScraper.js
@@ -1,39 +1,4 @@
 const cheerio = require("cheerio");
-const request = require("request-promise");
-const Nightmare = require("nightmare");
-
-const nightmare = Nightmare({ show: true });
-const url = "https://www.indeed.com";
-
-nightmare
-  .goto(url)
-  .wait("body")
-  .click("input#text-input-what")
-  .type("input#text-input-what", "Web Developer")
-  .click("input#text-input-where")
-  .type("input#text-input-where", "")
-  .click("button.icl-WhatWhere-button")
-  .wait("div.jobsearch-SerpJobCard")
-  .evaluate(() => document.querySelector("body").innerHTML)
-  .end()
-  .then(response => {
-    console.log(getData(response));
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
-let getData = html => {
-    data = [];
-    const $ = cheerio.load(html)
-    $('div.jobsearch-SerpJobCard').each((row, raw_element) => {
-        let title = $(raw_element).find('div.title a').attr('title');
-        let link = $(raw_element).find('div.title a').attr('href')
-        let company = $(raw_element).find('div.sjcl div span.company').text();
-        let location = $(raw_element).find('div.sjcl div.recJobLoc').attr('data-rc-loc')
-        let description = $(raw_element).find('div.summary ul li').text()
-        let date = $(raw_element).find('span.date').text()
-
 const Nightmare = require("nightmare");
 const fs = require('fs');
 
@@ -56,7 +21,7 @@ module.exports = {
         if (title) {
           data.push({
             title: title,
-            link: "https://www.indeed.com" + link,
+            link: "https://www.indeed.com/" + link,
             company: company,
             location: location,
             description: description,
@@ -81,28 +46,6 @@ module.exports = {
       .catch(err => {
         console.log(err);
       });
-  
     nightmare.end();
-
   }
 };
-
-        if(title) {
-            data.push({
-                title: title,
-                link: 'https://www.indeed.com' + link,
-                company: company,
-                location: location,
-                description: description,
-                date: date
-
-            })
-        }
-    })
-    return data
-}
-
-module.exports = {
-    theData: theData
-}
-
